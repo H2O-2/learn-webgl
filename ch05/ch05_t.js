c0ea74acc69fedb9bd8563798c67612606d97e4c
H2O-2 <1064737282@qq.com>
2018-01-16 00:04:41 -0800

    Chapter 5

--- ch05/ch05_t.js
+++ ch05/ch05_t.js
@@ -8,12 +8,48 @@ var VSHADER_SOURCE =
 '}\n';
 
 var FSHADER_SOURCE =
+'#ifdef GL_ES\n' +
+'precision mediump float;\n' +
+'#endif\n' +
 'uniform sampler2D u_Sampler;\n' +
 'varying vec2 v_TexCoord;\n' +
 'void main() {\n' +
 '   gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n' +
 '}\n';
 
+function main() {
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Set the vertex information
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Set texture
+  if (!initTextures(gl, n)) {
+    console.log('Failed to intialize the texture.');
+    return;
+  }
+}
 
 function initVertexBuffers(gl) {
     var verticesTexCoords = new Float32Array([
@@ -32,7 +68,7 @@ function initVertexBuffers(gl) {
     }
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexTexCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexTexCoordBuffer, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
 
     var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
@@ -52,4 +88,49 @@ function initVertexBuffers(gl) {
     gl.enableVertexAttribArray(a_TexCoord);
 
     return n;
+}
+
+function initTextures(gl, n) {
+    var texture = gl.createTexture();
+    if (!texture) {
+        console.log('Failed to create the texture object');
+        return false;
+    }
+
+    // Get the storage location of u_Sampler
+    var u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+    if (!u_Sampler) {
+        console.log('Failed to get the storage location of u_Sampler');
+        return false;
+    }
+
+    var image = new Image();
+    if (!image) {
+        console.log('Failed to create the image object');
+        return false;
+    }
+
+    image.onload = function() {
+        loadTexture(gl, n, texture, u_Sampler, image);
+    };
+    image.src = 'sky.jpg';
+
+    return true;
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image) {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+    // Enable texture unit0
+    gl.activeTexture(gl.TEXTURE0);
+    // Bind the texture object to the target
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // Set the texture parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    // Set the texture image
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+    // Set the texture unit 0 to the sampler
+    gl.uniform1i(u_Sampler, 0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
 }
